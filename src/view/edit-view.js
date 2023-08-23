e25dbc104cadc7ddff091e4fdc493c7f7814e909
Ryu <privetslavka@yandex.com>
2023-08-23 08:20:32 +0300

    Добавляет базовый функционал по редактированию точки маршрута

--- src/view/edit-view.js
+++ src/view/edit-view.js
@@ -2,19 +2,32 @@ import AbstractView from '@src/framework/view/abstract-view';
 import { createEditTemplate } from '@src/template/form/form-template';
 
 export default class EditView extends AbstractView {
-  constructor({ event, eventDestination, typeOffers }) {
+  #event = {};
+  #eventDestination = {};
+  #typeOffers = [];
+  #handleFormSubmit = null;
+
+  constructor({ event, eventDestination, typeOffers, onFormSubmit }) {
     super();
 
-    this.event = event;
-    this.eventDestination = eventDestination;
-    this.typeOffers = typeOffers;
+    this.#event = event;
+    this.#eventDestination = eventDestination;
+    this.#typeOffers = typeOffers;
+
+    this.#handleFormSubmit = onFormSubmit;
+    this.element.addEventListener('submit', this.#formSubmitHandler);
   }
 
   get template() {
     return createEditTemplate({
-      event: this.event,
-      eventDestination: this.eventDestination,
-      typeOffers: this.typeOffers
+      event: this.#event,
+      eventDestination: this.#eventDestination,
+      typeOffers: this.#typeOffers
     });
   }
+
+  #formSubmitHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleFormSubmit();
+  };
 }
--- src/view/event-view.js
+++ src/view/event-view.js
@@ -2,19 +2,32 @@ import AbstractView from '@src/framework/view/abstract-view';
 import { createEventTemplate } from '@src/template/event-template';
 
 export default class EventView extends AbstractView {
-  constructor({ event, eventDestination, typeOffers }) {
+  #event = {};
+  #eventDestination = {};
+  #typeOffers = [];
+  #handleEditClick = null;
+
+  constructor({ event, eventDestination, typeOffers, onEditClick }) {
     super();
 
-    this.event = event;
-    this.eventDestination = eventDestination;
-    this.typeOffers = typeOffers;
+    this.#event = event;
+    this.#eventDestination = eventDestination;
+    this.#typeOffers = typeOffers;
+
+    this.#handleEditClick = onEditClick;
+    this.element.querySelector('.event__rollup-btn').addEventListener('click', this.#editClickHandler);
   }
 
   get template() {
     return createEventTemplate({
-      event: this.event,
-      eventDestination: this.eventDestination,
-      typeOffers: this.typeOffers
+      event: this.#event,
+      eventDestination: this.#eventDestination,
+      typeOffers: this.#typeOffers
     });
   }
+
+  #editClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleEditClick();
+  };
 }
